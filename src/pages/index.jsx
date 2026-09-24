ba0b990d080b39de7b3dd0c59d31990c9a5eadef
Sinuhe Maceda <sinuhe.dev@gmail.com>
2026-09-24 02:21:49 -0600

    updated

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -39,20 +39,16 @@ export default function App() {
     functions
   )
 
-  const { state, fx } = pages
   const qs = useQueryString()
+  const { state, fx } = pages
   const viewTransitionRef = useRef()
+
   const Page = usePage({
-    hash: qs.hash,
+    hashPage: qs.hash,
     homePage: env.HOME_PAGE,
-    importPage: (path) => {
-      const currentPage = PAGES[path.join('/')] ?? PAGES.notFound
-      return currentPage()
-    },
-    viewTransition: {
-      ref: viewTransitionRef,
-      name: env.VIEW_TRANSITION_NAME
-    }
+    currentPage: (path) => PAGES[path.join('/')] ?? PAGES.notFound,
+    viewTransition: viewTransitionRef,
+    viewTransitionName: env.VIEW_TRANSITION_NAME
   })
 
   useEffect(() => {
